c3c2a1095842f4cae8adf83900ae5ae58caf56ce
Pelekanchik <131200784+Pelekanchik@users.noreply.github.com>
2024-02-28 20:20:03 +0200

    debug toolbar add додали

--- app/urls.py
+++ app/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from app.settings import DEBUG
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,3 +25,7 @@ urlpatterns = [
     path('catalog/', include('tovaru.urls', namespace='catalog')),
 ]
 
+if DEBUG:
+    urlpatterns += [
+        path("__debug__/", include("debug_toolbar.urls")),
+        ]
