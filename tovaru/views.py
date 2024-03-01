632a80ffdbeb122151c8ac714928a2064e2a833a
Pelekanchik <131200784+Pelekanchik@users.noreply.github.com>
2024-03-01 14:40:26 +0200

    Слаг (slug) конвертор для продукту

--- tovaru/views.py
+++ tovaru/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.template import context
 
 from tovaru.models import Products 
 
@@ -13,5 +14,12 @@ def catalog(request):
     return render(request, 'tovaru/catalog.html', context)
 
 
-def product(request):
-    return render(request, 'tovaru/product.html')
+def product(request, product_slug):
+
+    product = Products.objects.get(slug=product_slug)     #ми отримуємо всю інформацію по конкретному товару по slug
+
+    context = {
+        'product': product
+    }
+    
+    return render(request, 'tovaru/product.html', context=context)
