dcc21ccfc239b90b5c937eace62dbb8fc78f26e8
Pelekanchik <131200784+Pelekanchik@users.noreply.github.com>
2024-03-02 08:09:02 +0200

    Нормальний варіант пагінації

--- tovaru/views.py
+++ tovaru/views.py
@@ -4,7 +4,9 @@ from django.shortcuts import get_list_or_404, render
 from tovaru.models import Products 
 
 
-def catalog(request, category_slug, page=1):
+def catalog(request, category_slug):
+
+    page = request.GET.get('page', 1)
 
     if category_slug == 'all':
         tovaru = Products.objects.all()
@@ -13,7 +15,7 @@ def catalog(request, category_slug, page=1):
 
 
     paginator = Paginator(tovaru, 6)
-    current_page = paginator.page(page)
+    current_page = paginator.page(int(page))
 
     context = {
         'title': 'home - Каталог',
