435686929f1c613ccd6136f2af3c64e933032eb8
Pelekanchik <131200784+Pelekanchik@users.noreply.github.com>
2024-03-04 10:34:04 +0200

    simple search created простий поісковік міняємо базу данних

--- tovaru/views.py
+++ tovaru/views.py
@@ -2,43 +2,47 @@ from django.core.paginator import Paginator
 from django.shortcuts import get_list_or_404, render
 
 
-from tovaru.models import Products 
+from tovaru.models import Products
+from tovaru.utils import q_search
 
 
-def catalog(request, category_slug):
+def catalog(request, category_slug=None):
 
-    page = request.GET.get('page', 1)
-    on_sale = request.GET.get('on_sale', None)
-    order_by = request.GET.get('order_by', None)
+    page = request.GET.get("page", 1)
+    on_sale = request.GET.get("on_sale", None)
+    order_by = request.GET.get("order_by", None)
+    query = request.GET.get("q", None)
 
-    if category_slug == 'all':
+    if category_slug == "all":
         tovaru = Products.objects.all()
+    elif query:
+        tovaru = q_search(query)
     else:
         tovaru = get_list_or_404(Products.objects.filter(category__slug=category_slug))
 
     if on_sale:
         tovaru = tovaru.filter(discount__gt=0)
 
-    if order_by and order_by != 'default':    
+    if order_by and order_by != "default":
         tovaru = tovaru.order_by(order_by)
 
     paginator = Paginator(tovaru, 6)
     current_page = paginator.page(int(page))
 
     context = {
-        'title': 'home - Каталог',
-        'tovaru': current_page,
+        "title": "home - Каталог",
+        "tovaru": current_page,
         "slug_url": category_slug,
     }
-    return render(request, 'tovaru/catalog.html', context)
+    return render(request, "tovaru/catalog.html", context)
 
 
 def product(request, product_slug):
 
-    product = Products.objects.get(slug=product_slug)     #ми отримуємо всю інформацію по конкретному товару по slug
+    product = Products.objects.get(
+        slug=product_slug
+    )  # ми отримуємо всю інформацію по конкретному товару по slug
 
-    context = {
-        'product': product
-    }
-    
-    return render(request, 'tovaru/product.html', context=context)
+    context = {"product": product}
+
+    return render(request, "tovaru/product.html", context=context)
