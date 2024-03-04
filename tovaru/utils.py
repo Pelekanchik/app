fe7fb206d0290a35c672d6eebef5138eddd16699
Pelekanchik <131200784+Pelekanchik@users.noreply.github.com>
2024-03-04 15:23:12 +0200

    Full text search well-done

--- tovaru/utils.py
+++ tovaru/utils.py
@@ -1,5 +1,10 @@
 from django.db.models import Q
-from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.contrib.postgres.search import (
+    SearchVector,
+    SearchQuery,
+    SearchRank,
+    SearchHeadline,
+)
 from tovaru.models import Products
 
 
@@ -9,10 +14,30 @@ def q_search(query):
     
     vector = SearchVector("name", "description")
     query = SearchQuery(query)
-       
-    return Products.objects.annotate(rank=SearchRank(vector, query)).order_by("-rank")
 
+    result = (
+        Products.objects.annotate(rank=SearchRank(vector, query))
+        .filter(rank__gt=0)
+        .order_by("-rank")
+    )
 
+    result = result.annotate(
+        headline=SearchHeadline(
+            "name",
+            query,
+            start_sel='<span style="background-color: yellow;">',
+            stop_sel="</span>",
+        )
+    )
+    result = result.annotate(
+        bobyline=SearchHeadline(
+            "description",
+            query,
+            start_sel='<span style="background-color: yellow;">',
+            stop_sel="</span>",
+        )
+    )
+    return result
 
     # keywords = [word for word in query.split() if len(word) > 2 ]
 
@@ -22,4 +47,4 @@ def q_search(query):
     #     q_objects |= Q(description__icontains=token)
     #     q_objects |= Q(name__icontains=token)
 
-    # return Products.objects.filter(q_objects)    
+    # return Products.objects.filter(q_objects)
