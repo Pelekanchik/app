6740a182365a45b013c435932a1b2d5c8b74e68d
Pelekanchik <131200784+Pelekanchik@users.noreply.github.com>
2024-03-06 19:18:32 +0200

    Add to cart view, refactor included cart templates (Все працює добреб, поісковік ні)

--- carts/views.py
+++ carts/views.py
@@ -1,10 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from carts.models import Cart
 
-def cart_add(request, product_id):
-    ...
+from tovaru.models import Products
 
-def cart_change(request, product_id):
+
+def cart_add(request, product_slug):
+    
+    product = Products.objects.get(slug=product_slug)
+
+    if request.user.is_authenticated:
+        carts = Cart.objects.filter(user=request.user, product=product)
+
+        if carts.exists():                                     # якщо товар є в корзині добавиться ще один додатково
+            cart = carts.first()
+            if cart:
+                cart.quantity += 1
+                cart.save()
+        else:
+            Cart.objects.create(user=request.user, product=product, quantity=1) 
+
+    return redirect(request.META['HTTP_REFERER'])
+
+def cart_change(request, product_slug):
     ...    
 
-def cart_remove(request, product_id):
+def cart_remove(request, product_slug):
     ...
--- users/views.py
+++ users/views.py
@@ -17,8 +17,9 @@ def login(request):
             if user:
                 auth.login(request, user)
                 messages.success(request, f"{username}, Ви увійшли в акаунт")
-
-                if request.POST.get('next', None):
+                
+                redirect_page = request.POST.get('next',None)
+                if redirect_page and redirect_page != reverse('user:logout'):
                     return HttpResponseRedirect(request.POST.get('next'))
                 
                 return HttpResponseRedirect(reverse('main:index'))
