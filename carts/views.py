4e97e31da6b642e81823dcc616f750548ed7694d
Pelekanchik <131200784+Pelekanchik@users.noreply.github.com>
2024-03-07 13:06:24 +0200

    Add-tocart ajax request and response (все працює окрім поісковіка)

--- carts/views.py
+++ carts/views.py
@@ -1,17 +1,22 @@
+from django.http import JsonResponse
 from django.shortcuts import redirect, render
+from django.template.loader import render_to_string
 from carts.models import Cart
+from carts.utils import get_user_carts
 
 from tovaru.models import Products
 
 
-def cart_add(request, product_slug):
+def cart_add(request):
     
-    product = Products.objects.get(slug=product_slug)
+    product_id = request.POST.get("product_id")
+
+    product = Products.objects.get(id=product_id)
 
     if request.user.is_authenticated:
         carts = Cart.objects.filter(user=request.user, product=product)
 
-        if carts.exists():                                     # якщо товар є в корзині добавиться ще один додатково
+        if carts.exists():                                   
             cart = carts.first()
             if cart:
                 cart.quantity += 1
@@ -19,13 +24,23 @@ def cart_add(request, product_slug):
         else:
             Cart.objects.create(user=request.user, product=product, quantity=1) 
 
-    return redirect(request.META['HTTP_REFERER'])
+    user_cart = get_user_carts(request)
+    cart_items_html = render_to_string(
+        "carts/includes/included_cart.html", {"carts": user_cart}, request=request)
+
+    response_data = {
+        "message": "Товар добавлено до кошика",
+        "cart_items_html": cart_items_html,
+    }
+
+    return JsonResponse(response_data)
+
 
 def cart_change(request, product_slug):
     ...    
 
 def cart_remove(request, cart_id):
-    
     cart = Cart.objects.get(id=cart_id)
     cart.delete()
+    
     return redirect(request.META['HTTP_REFERER'])
