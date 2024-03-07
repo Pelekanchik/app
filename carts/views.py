fc158626d7e938a5a461c1f1a6d05967d3492f03
Pelekanchik <131200784+Pelekanchik@users.noreply.github.com>
2024-03-07 13:23:29 +0200

    Remove from cart ajax and json response (видалення товару з корзини)

--- carts/views.py
+++ carts/views.py
@@ -39,8 +39,22 @@ def cart_add(request):
 def cart_change(request, product_slug):
     ...    
 
-def cart_remove(request, cart_id):
+def cart_remove(request):
+
+    cart_id = request.POST.get("cart_id")
+
     cart = Cart.objects.get(id=cart_id)
+    quantity = cart.quantity
     cart.delete()
+
+    user_cart = get_user_carts(request)
+    cart_items_html = render_to_string(
+        "carts/includes/included_cart.html", {"carts": user_cart}, request=request)
     
-    return redirect(request.META['HTTP_REFERER'])
+    response_data = {
+        "message": "Товар видалено",
+        "cart_items_html": cart_items_html,
+        "quantity_deleted": quantity,
+    }
+
+    return JsonResponse(response_data)
