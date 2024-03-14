c964b6447da55d256bb35e40c6a5e099655479fc
Pelekanchik <131200784+Pelekanchik@users.noreply.github.com>
2024-03-14 19:00:31 +0200

    Останній варіант

--- tovaru/views.py
+++ tovaru/views.py
@@ -30,7 +30,7 @@ def catalog(request, category_slug=None):
     current_page = paginator.page(int(page))
 
     context = {
-        "title": "Home - Каталог",
+        "title": "Домашня - Каталог",
         "tovaru": current_page,
         "slug_url": category_slug
     }
